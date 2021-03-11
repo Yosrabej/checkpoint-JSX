cb14f67182869cf4660e4206e91eedc8c6346f60
Yosra Bejaoui <bejaoui.yosra92@gmail.com>
2021-03-11 17:33:10 +0100

    checkpoint JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import tree from "./treeimg.png";
+import "./style.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div style={{ border: "solid 1px black", maxwidth: "100vw" }}>
+                <h1 className="title red">Your name here</h1>
+
+                <br />
+
+                <img src={tree} />
+
+                <br />
+
+                <img src="/images.jpg" />
+            </div>
+
+            <video width="320" height="240" controls>
+                <source src="/myVideo.mp4" type="video/mp4" />
+            </video>
+        </div>
+    );
 }
 
 export default App;
